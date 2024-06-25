4a1fc554aeda7822f95a8caeb79e98431a996174
Florian Kiwit <florian.kiwit@bmw.de>
2024-06-25 07:41:53 -0700

    Add minimal exmample for image compression

--- examples/pq_ray.py
+++ examples/pq_ray.py
@@ -4,6 +4,9 @@ import sys
 import socket
 import time
 import pennylane as qml
+from pennylane import numpy as np
+from scipy.optimize import minimize
+
 import ray
 import pilot
 from pilot.pilot_compute_service import PilotComputeService
@@ -62,6 +65,49 @@ def run_circuit():
     return (end-start)
 
 @ray.remote
+def fit_circuit_to_target():
+    # Add to requirements.txt: jax, scipy
+    start = time.time()    
+
+    wires = 5
+    layers = 2
+    dev = qml.device('default.qubit', wires=wires)
+    @qml.qnode(dev)
+    def circuit(parameters):
+        parameters = parameters.reshape((layers, wires, 3))
+        qml.StronglyEntanglingLayers(weights=parameters, wires=range(wires))
+        return qml.state()
+
+    def loss_fn(params, target):
+        return 1 - np.sum(np.abs(target.conj() * circuit(params)))
+
+    grad_fn = qml.grad(loss_fn, argnum=0)
+    weights = qml.numpy.random.random(size=layers*wires*3).flatten()
+
+    # Generate target state, this would be replaced by the target state that reprents the image
+    target = np.random.random([2 ** wires]) + 1j * np.random.random([2 ** wires])
+    target /= np.linalg.norm(target)
+    target = np.array(target, requires_grad=False)
+
+    losses = [float(loss_fn(weights, target))]
+    def callback(x):
+        current_loss = float(loss_fn(x, target))
+        losses.append(current_loss)
+
+    # Optimize
+    res = minimize(
+        fun=lambda param: loss_fn(param, target),
+        callback=callback,
+        method='BFGS',
+        x0=weights,
+        options={"return_all": True, 'maxiter': 100, "xrtol": 0, "gtol": 0},
+        jac=lambda param: grad_fn(param, target)
+        )
+
+    end=time.time()
+    return (end-start)
+
+@ray.remote
 def f(x):
     return x * x    
 
@@ -80,12 +126,13 @@ if __name__ == "__main__":
 
     try:
         # Start Pilot
-        ray_pilot = start_pilot(pilot_compute_description_ray_gpu)
+        ray_pilot = start_pilot(pilot_compute_description_ray_cpu)
         
         # Get Dask client details
         ray_client = ray_pilot.get_client()
         
         with ray_client:
+            print(ray.get([fit_circuit_to_target.remote() for i in range(10)]))
             print(ray.get([f.remote(i) for i in range(10)]))
             print(ray.get([run_circuit.remote() for i in range(10)]))
     
